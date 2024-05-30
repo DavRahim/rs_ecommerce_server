968c7d0c73c28acbf0e0d0b58eb2ad3c92d5150d
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-05-30 23:45:02 +0600

    some fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 // middleware
 const corsOptions = {
-  origin: process.env.MODE === "pro" ? "https://rs-ecommerce-server.onrender.com" : ["http://localhost:5173", "http://localhost:5174"],
+  origin: process.env.MODE === "pro" ? "https://rs-ecommerce-five.vercel.app" : ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   optionSuccessStatus: 200,
 };
